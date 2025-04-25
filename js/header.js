e838bf77338485867ba2e87411803fa0e184e697
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2025-04-25 20:15:00 -0300

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,33 @@
+const btnCadastrar = document.getElementById('btnNovaMusica')
+const btnListar = document.getElementById('listaMusicas')
+
+const sectionNova = document.getElementById('novaMusica')
+const sectionLista = document.getElementById('musicas')
+const sectionEditar = document.getElementById('editar')
+
+function editarSair() {
+    document.getElementById('editar').style.display = 'none'
+}
+
+function musicOn() {
+    btnCadastrar.classList.remove('on')
+    btnListar.classList.add('on')
+    
+    sectionNova.style.display = 'none'
+    sectionLista.style.display = 'flex'
+    editarSair();
+}
+
+btnCadastrar.addEventListener('click', ()=>{
+
+    btnCadastrar.classList.add('on')
+    btnListar.classList.remove('on')
+    
+    sectionNova.style.display = 'flex'
+    sectionLista.style.display = 'none'
+    editarSair();
+})
+
+btnListar.addEventListener('click', ()=>{
+    musicOn()
+})
